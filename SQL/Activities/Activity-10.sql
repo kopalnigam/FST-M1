67502ca926b0d25ad083fbc2cf194a3991d2fce2
kopalnigam <140591628+kopalnigam@users.noreply.github.com>
2023-09-25 16:02:00 +0530

    Add files via upload

---
+++ SQL/Activities/Activity-10.sql
@@ -0,0 +1,15 @@
+select * from salesman;
+select * from Customers;
+select * from orders;
+
+SELECT * FROM orders
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+select * from orders where salesman_id in(select salesman_id where salesman_city = 'Newyork' from salesman );
+
+SELECT grade, COUNT(*) FROM customers
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders
+WHERE salesman_id IN( SELECT salesman_id FROM salesman
+WHERE commission=( SELECT MAX(commission) FROM salesman));
